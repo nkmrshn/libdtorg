4d4be7daba84135f26e242ab4f1bdc35c6569bd1
nkmrshn <nkmrshn@gmail.com>
2013-05-08 04:43:46 +0900

    changed from unsigned char to char

--- libdtorg.h
+++ libdtorg.h
@@ -2,10 +2,10 @@
 #define LIBDTORG_H
 
 typedef struct DTORG_FILELIST_tag {
-  char *filename;                     //  Filename
-  unsigned char *date_time_original;  //  DateTimeOriginal
-  struct DTORG_FILELIST_tag *next;    //  next struct pointer
-  struct DTORG_FILELIST_tag *last;    //  last struct pointer
+  char *filename;                   //  Filename
+  char *date_time_original;         //  DateTimeOriginal
+  struct DTORG_FILELIST_tag *next;  //  next struct pointer
+  struct DTORG_FILELIST_tag *last;  //  last struct pointer
 } DTORG_FILELIST;
 
 DTORG_FILELIST *dtorg_read_dir(char *);
