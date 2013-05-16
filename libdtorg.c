fd3eacf3407872cbc2f6d3f6f6f3373bf12c64ab
nkmrshn <nkmrshn@gmail.com>
2013-05-16 14:27:30 +0900

    bug fix

--- libdtorg.c
+++ libdtorg.c
@@ -4,56 +4,16 @@
 #include <math.h>
 #include <dirent.h>
 #include <regex.h>
-#include <stdbool.h>
+#include "util.h"
 #include "libdtorg.h"
 
 #define HEADER_BYTES 12
 
 const unsigned char *FILE_FMT = "^[a-zA-Z0-9]{1,8}\\.(JPG|TIF)$";
 
-const unsigned char SOI[] = {0xff, 0xd8};
-const unsigned char APP1_MARKER[] = {0xff, 0xe1};
-const unsigned char EXIF_IDENT_CODE[] = {0x45, 0x78, 0x69, 0x66, 0x00, 0x00};
-const unsigned char TIFF_LITTLE_ENDIAN[] = {0x49, 0x49};
-const unsigned char TIFF_BIG_ENDIAN[] = {0x4d, 0x4d};
-
 const unsigned char EXIF_IFD_POINTER_TAG[] = {0x87, 0x69};
 const unsigned char DATE_TIME_ORIGINAL_TAG[] = {0x90, 0x03};
-
-unsigned long todecimal(unsigned char *str, size_t s)
-{
-  int i;
-  unsigned long x = 0;
-
-  for(i = 0; i < s; i++) {
-    x += str[i] * (unsigned long)pow(256, s - i - 1);
-  }
-
-  return x;
-}
-
-int strrev(char *str, size_t s)
-{
-  int i;
-  char *tmp;
   
-  if((tmp = malloc(sizeof(char) * s)) == NULL)
-    return 1;
-  
-  for(i = 0; i < s; i++) {
-    tmp[i] = str[i];
-  }
-  
-  for(i = 1; i <= s; i++) {
-    str[i - 1] = tmp[s - i];
-  }
-
-  free(tmp);
-  tmp = NULL;
-
-  return 0;
-}
-
 DTORG_FILELIST *dtorg_read_dir(char *path, int *count)
 {
   DTORG_FILELIST *list = NULL, *tmp;
@@ -121,30 +81,6 @@ DTORG_FILELIST *dtorg_read_dir(char *path, int *count)
   return list;
 }
 
-void dtorg_seek(FILE *fp, long offset, int whence, bool *is_valid)
-{
-  if(is_valid && fseek(fp, offset, whence) != 0)
-    is_valid = false;
-}
-
-void dtorg_read(void *ptr, size_t s, FILE *fp, bool *is_valid)
-{
-  if(is_valid && fread(ptr, sizeof(unsigned char), s, fp) != s)
-    is_valid = false;
-}
-
-void dtorg_reverse(bool is_little_endian, void *ptr, size_t s, bool *is_valid)
-{
-  if(is_valid && is_little_endian && strrev(ptr, s) != 0)
-    is_valid = false;
-}
-
-void dtorg_check(const unsigned char *compare, void *ptr, size_t s, bool *is_valid)
-{
-  if(is_valid && strncmp(compare, ptr, s) != 0)
-    is_valid = false;
-}
-
 void dtorg_read_list(DTORG_FILELIST *list)
 {
   DTORG_FILELIST *tmp = list;
@@ -159,6 +95,7 @@ void dtorg_read_list(DTORG_FILELIST *list)
   for(tmp = list; tmp != NULL; tmp = tmp->next) {
     //  Initialize
     is_valid = true;  //  assume it is valid
+    is_little_endian = NULL;
     dtorg_offset = 0;
     dtorg_count = 0;
     entry_count = 0;
@@ -166,68 +103,37 @@ void dtorg_read_list(DTORG_FILELIST *list)
     if((fp = fopen(tmp->filename, "rb")) == NULL)
       continue;
 
-    //  SOI
-    dtorg_read(buffer, 2, fp, &is_valid);
-    dtorg_check(SOI, buffer, 2, &is_valid);
-
-    //  APP1 Marker
-    dtorg_read(buffer, 2, fp, &is_valid);
-    dtorg_check(APP1_MARKER, buffer, 2, &is_valid);
-
-    //  EXIF Identifier Code
-    dtorg_seek(fp, 2, SEEK_CUR, &is_valid);  //  Skip (APP1 Length)
-    dtorg_read(buffer, 6, fp, &is_valid);
-    dtorg_check(EXIF_IDENT_CODE, buffer, 6, &is_valid);
-
-    //  Byte Order
-    dtorg_read(buffer, 2, fp, &is_valid);
-
-    if(is_valid && strncmp(TIFF_LITTLE_ENDIAN, buffer, 2) == 0)
-      is_little_endian = true;
-    else if(is_valid && strncmp(TIFF_BIG_ENDIAN, buffer, 2) == 0)
-      is_little_endian = false;
-    else
-      is_valid = false;
-
-    //  0th IFD Offset
-    dtorg_seek(fp, 2, SEEK_CUR, &is_valid);  //  Skip 0x002A
-    dtorg_read(buffer, 4, fp, &is_valid);
-    dtorg_reverse(is_little_endian, buffer, 4, &is_valid);
-
-    //  0th IFD
-    dtorg_seek(fp, todecimal(buffer, 4) - 8, SEEK_CUR, &is_valid);
+    //  EXIF Header
+    read_exif_header(fp, buffer, &is_valid, &is_little_endian);
 
     //  0th IFD Entry Count
-    dtorg_read(buffer, 2, fp, &is_valid);
-    dtorg_reverse(is_little_endian, buffer, 2, &is_valid);
-
     if(is_valid)
       entry_count = todecimal(buffer, 2);
 
     //  0th IFD Entry
     for(i = 0; is_valid && i < entry_count; i++) {
       //  Read Tag
-      dtorg_read(buffer, 2, fp, &is_valid);
-      dtorg_reverse(is_little_endian, buffer, 2, &is_valid);
+      vread(buffer, 2, fp, &is_valid);
+      vrev(buffer, 2, &is_valid, &is_little_endian);
 
       //  EXIF IFD Pointer Tag
-      if(is_valid && strncmp(EXIF_IFD_POINTER_TAG, buffer, 2) == 0)
+      if(is_valid && memcmp(EXIF_IFD_POINTER_TAG, buffer, 2) == 0)
         break;
 
-      dtorg_seek(fp, 10, SEEK_CUR, &is_valid); // Skip (Type, Count, Offset)
+      vseek(fp, 10, SEEK_CUR, &is_valid); // Skip (Type, Count, Offset)
     }
 
     //  EXIF IFD Offset
-    dtorg_seek(fp, 6, SEEK_CUR, &is_valid);  //  Skip (Type, Count)
-    dtorg_read(buffer, 4, fp, &is_valid);
-    dtorg_reverse(is_little_endian, buffer, 4, &is_valid);
+    vseek(fp, 6, SEEK_CUR, &is_valid);  //  Skip (Type, Count)
+    vread(buffer, 4, fp, &is_valid);
+    vrev(buffer, 4, &is_valid, &is_little_endian);
 
     //  EXIF IFD
-    dtorg_seek(fp, todecimal(buffer, 4) + HEADER_BYTES, SEEK_SET, &is_valid);
+    vseek(fp, todecimal(buffer, 4) + HEADER_BYTES, SEEK_SET, &is_valid);
 
     //  EXIF IFD Entry Count
-    dtorg_read(buffer, 2, fp, &is_valid);
-    dtorg_reverse(is_little_endian, buffer, 2, &is_valid);
+    vread(buffer, 2, fp, &is_valid);
+    vrev(buffer, 2, &is_valid, &is_little_endian);
     
     if(is_valid)
       entry_count = todecimal(buffer, 2); 
@@ -235,14 +141,14 @@ void dtorg_read_list(DTORG_FILELIST *list)
     //  EXIF IFD Entry
     for(i = 0; is_valid && i < entry_count; i++) {
       //  Read Tag
-      dtorg_read(buffer, 2, fp, &is_valid);
-      dtorg_reverse(is_little_endian, buffer, 2, &is_valid);
+      vread(buffer, 2, fp, &is_valid);
+      vrev(buffer, 2, &is_valid, &is_little_endian);
 
-      if(is_valid && strncmp(DATE_TIME_ORIGINAL_TAG, buffer, 2) == 0) {
+      if(is_valid && memcmp(DATE_TIME_ORIGINAL_TAG, buffer, 2) == 0) {
         //  Date Time Orginal Count
-        dtorg_seek(fp, 2, SEEK_CUR, &is_valid);  //  Skip (Type)
-        dtorg_read(buffer, 4, fp, &is_valid);
-        dtorg_reverse(is_little_endian, buffer, 4, &is_valid);
+        vseek(fp, 2, SEEK_CUR, &is_valid);  //  Skip (Type)
+        vread(buffer, 4, fp, &is_valid);
+        vrev(buffer, 4, &is_valid, &is_little_endian);
 
         if(is_valid) {
           dtorg_count = todecimal(buffer, 4);
@@ -253,21 +159,21 @@ void dtorg_read_list(DTORG_FILELIST *list)
         }
 
         //  Date Time Original Offset
-        dtorg_read(buffer, 4, fp, &is_valid);
-        dtorg_reverse(is_little_endian, buffer, 4, &is_valid);
+        vread(buffer, 4, fp, &is_valid);
+        vrev(buffer, 4, &is_valid, &is_little_endian);
 
         if(is_valid)
           dtorg_offset = todecimal(buffer, 4);
 
         break;
       } else 
-        dtorg_seek(fp, 10, SEEK_CUR, &is_valid);  //  Skip (Type, Count, Offset)
+        vseek(fp, 10, SEEK_CUR, &is_valid);  //  Skip (Type, Count, Offset)
     }
 
     // Date Time Original Value
     if(is_valid && dtorg_count > 0) {
-      dtorg_seek(fp, dtorg_offset + HEADER_BYTES, SEEK_SET, &is_valid);  // offset from beginning
-      dtorg_read(tmp->date_time_original, dtorg_count, fp, &is_valid);
+      vseek(fp, dtorg_offset + HEADER_BYTES, SEEK_SET, &is_valid);  // offset from beginning
+      vread(tmp->date_time_original, dtorg_count, fp, &is_valid);
     }
 
     if(fp != NULL)
